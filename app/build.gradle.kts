17335ec80e1a600cfeee3dd8e49673f06330716a
Thawee Prakaipetch <thawee.p@kbtg.tech>
2026-06-12 09:22:09 +0700

    chore: Bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "apincer.mobile.tradings"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
